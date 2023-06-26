18209b1a96883d9e59de8c2c0cd360730f8ba845
adrian <adrian@zerobeta.xyz>
2023-06-27 02:25:28 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Title from "./Title";
+import {tours} from '../data';
+import Tour from "./Tour";
+
+const Tours = () => {
+    return (
+        <div>
+            <section className="section" id="tours">
+               <Title title={'featured'} subTitle={'tours'}/>
+
+                <div className="section-center featured-center">
+                    {tours.map((tour) => {
+                        return <Tour key={tour.id} {...tour}/>;
+                    })}
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Tours;
